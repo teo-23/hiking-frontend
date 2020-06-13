dc7b38454a26c9fed3f493e7640b1d6098c263d5
teo23 <matteoazzola23@gmail.com>
2020-06-13 16:19:31 +0200

    fixed login

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
+import trailService from '../../service/trail-service'
 import './searchResults.css'
 
-export default function SearchResults(props) {
-    return (
+class SearchResults extends Component {
+    
+    state = {
+        img: this.props.img,
+        name: this.props.name,
+        difficulty: this.props.difficulty,
+        service: new trailService()
+    }
 
-        <div class="wrapper">
+    addToFavorite = () => {
+        const trail = {
+            img: this.state.img,
+            name: this.state.name,
+            difficulty: this.state.difficulty,
+        }
+        this.state.service.addToFavorite(trail)
+    }
+
+    render() {
+        return (
+            <div class="wrapper">
             <div className="card-container">
                 <div className="card-image">
-                    <img src={props.img} alt=""/>
+                    <img src={this.props.img} onError={(e)=>{e.target.onerror = null; e.target.src="http://placekitten.com/g/200/200"}} alt="nature image"/>
                 </div>
+                {/* <div className="card-headers">
+                    <h5>trail:</h5>
+                    <h5>difficulty:</h5>
+                    <h5>summary:</h5>
+                </div> */}
                 <div className="card-info">
-                    <p>name: {props.name}</p>
-                    <p>difficulty: {props.difficulty}</p>
-                    <p>summary: {props.summary}</p>
+                    <h7>{this.props.name}</h7>
+                    <div id="difficulty">
+                        <h7>{this.props.difficulty}</h7>
+                    </div>
+                    
+                    <div id="summary">
+                        <h7>{this.props.summary}</h7>
+                    </div>
+                    
+                </div>
+                <div className="like">
+                    <button onClick={this.addToFavorite}>favorite</button>
                 </div>
             </div>
         </div>
-
-    )
+        );
+    }
 }
+
+export default SearchResults;

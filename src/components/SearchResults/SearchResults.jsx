8d1fa3fba706b583b3a65bb697a87df36cc5b224
MichielSnoek <michiel.snoek@gmail.com>
2020-06-17 17:28:01 +0200

    Some changes to searchResults and map sizing

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -24,39 +24,38 @@ class SearchResults extends Component {
     render() {
         return (
             <div className="wrapper">
-            <div className="card-container">
-                <div className="card-image">
-                    <img src={this.props.img} onError={(e)=>{e.target.onerror = null; e.target.src="http://placekitten.com/g/200/200"}} alt="nature"/>
-                </div>
-                {/* <div className="card-headers">
-                    <h5>trail:</h5>
-                    <h5>difficulty:</h5>
-                    <h5>summary:</h5>
-                </div> */}
-                <div className="card-info">
-                    <h6><strong>#{this.props.number}</strong> {this.props.name}</h6>
+                <div className="card-container">
 
-                    <div className="card-info-wrapper">
-                        <div id="difficulty">
-                            <h6>{this.props.difficulty}</h6>
+                    <div className="card-image">
+                        <img src={this.props.img} onError={(e)=>{e.target.onerror = null; e.target.src="http://placekitten.com/g/200/200"}} alt="nature"/>
+                    </div>
+
+                    <div className="card-info">
+                        <h6><strong>#{this.props.number}</strong> {this.props.name}</h6>
+
+                        <div className="card-info-wrapper">
+                            <div id="difficulty">
+                                <h6>{this.props.difficulty}</h6>
+                            </div>
+                            <div id="star-rating">
+                                <StarRating 
+                                stars={this.props.stars}
+                                />
+                            </div> 
                         </div>
-                        <div id="star-rating">
-                            <StarRating 
-                            stars={this.props.stars}
-                            />
-                        </div> 
+                        
+                        <div id="summary">
+                            <h6>{this.props.summary}</h6>
+                        </div>
+                        
                     </div>
-                    
-                    <div id="summary">
-                        <h6>{this.props.summary}</h6>
+
+                    <div className="like">
+                        <button onClick={this.addToFavorite}>favorite</button>
                     </div>
-                    
-                </div>
-                <div className="like">
-                    <button onClick={this.addToFavorite}>favorite</button>
+
                 </div>
             </div>
-        </div>
         );
     }
 }

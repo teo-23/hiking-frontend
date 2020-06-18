fe310b10c7f9d6d75de8d1e89357f3bf1ced9e6f
MichielSnoek <michiel.snoek@gmail.com>
2020-06-18 21:06:00 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import TrailsList from './components/trails/TrailsList';
 import Navbar from './components/navbar/Navbar';

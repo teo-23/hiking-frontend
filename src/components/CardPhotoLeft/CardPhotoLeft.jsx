aedb9d6db08eea07b2cef577a478a8876dfd7276
teo23 <matteoazzola23@gmail.com>
2020-06-23 18:59:33 +0200

    removed comments in return statement

--- src/components/CardPhotoLeft/CardPhotoLeft.jsx
+++ src/components/CardPhotoLeft/CardPhotoLeft.jsx
@@ -6,22 +6,7 @@ import HorizontalLine from '../HorizontalLine/HorizontalLine';
 export default function CardPhotoLeft() {
     return (
         <>
-        {/* <div className="row row-with-columns align-items-center">
-            <div className="col-6"><img className="photo-left" src="/theBeach.jpg" alt="landscape" /></div>
-            <div className="col-6 content-left-photo">
-            <p>Save, create, and share your favorite trails</p>
-                <div className="mb-2">
-                    <Button variant="success" size="lg" href="/signup" className="button button-home">
-                    Signup
-                    </Button>{' '}
-                    <Button variant="success" size="lg" href="/login" className="button button-home">
-                    Login
-                    </Button>
-                </div>
-            </div>
-        </div> */}
 
-        <br></br>
         <Card className="bg-dark text-white card">
             <Card.Img src="/homepage.jpg" alt="italian Alps" />
                 <Card.ImgOverlay>
--- src/components/HorizontalPhoto/HorizontalPhoto.jsx
+++ src/components/HorizontalPhoto/HorizontalPhoto.jsx
@@ -6,20 +6,6 @@ export default function HorizontalPhoto() {
     
     return (
         
-        // <Jumbotron fluid>
-        //     <Container className="jumbotron-fluid container-photo-central">
-        //     <div className="hero-title">
-        //         <h3>...walk towards your dreams</h3>
-        //     </div>   
-        //     </Container>
-        // </Jumbotron>
-        // {/* <div className="jumbotron-fluid">
-        // <div className="container container-photo-central">
-        //     <img className="Photo-central" src="/Chile_Paine.jpg" alt="central-main-img" fluid/>
-        //     {/* <h2 className="display-3">Hi, king...</h2> */}
-        //     {/* <p>...walk towards your dreams</p> */}
-        // </div>
-        // </div> */}
         <Carousel>
         <Carousel.Item>
             <img

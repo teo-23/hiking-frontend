f0476d63bce3dd7fe3d6a4622cea854f634c9537
teo23 <matteoazzola23@gmail.com>
2020-06-23 18:46:22 +0200

    some home fixes

--- src/components/CardPhotoLeft/CardPhotoLeft.jsx
+++ src/components/CardPhotoLeft/CardPhotoLeft.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './CardPhotoLeft.css';
-import { Button } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import HorizontalLine from '../HorizontalLine/HorizontalLine';
 
 export default function CardPhotoLeft() {
     return (
         <>
-        <div className="row row-with-columns align-items-center">
+        {/* <div className="row row-with-columns align-items-center">
             <div className="col-6"><img className="photo-left" src="/theBeach.jpg" alt="landscape" /></div>
             <div className="col-6 content-left-photo">
             <p>Save, create, and share your favorite trails</p>
@@ -19,7 +19,27 @@ export default function CardPhotoLeft() {
                     </Button>
                 </div>
             </div>
-        </div>
+        </div> */}
+
+        <br></br>
+        <Card className="bg-dark text-white card">
+            <Card.Img src="/homepage.jpg" alt="italian Alps" />
+                <Card.ImgOverlay>
+                    <Card.Title className="align-elements">Save, create, and share your favorite trails</Card.Title>
+                    {/* <Card.Text>
+                    This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.
+                    </Card.Text> */}
+                    {/* <Card.Text>Last updated 3 mins ago</Card.Text> */}
+                    <Button variant="success" size="lg" href="/signup" className="button button-home">
+                    Signup
+                    </Button>{' '}
+                    <Button variant="success" size="lg" href="/login" className="button button-home">
+                    Login
+                    </Button>
+                </Card.ImgOverlay>
+        </Card>
+
         <br></br>
         <HorizontalLine />
         <h1 className="title-sliding">Chosen by more than a million hikers </h1>
